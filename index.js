80f480c91b8d1740661f1e85ee0341ac9c306506
FaetterP <56697273+FaetterP@users.noreply.github.com>
2022-11-12 21:58:07 +0300

    Изменена база данных

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function isFolder(path) {
 
     for (const key of Object.keys(ret)) {
         console.log(key)
-        const dbRef = ref(getDatabase(), `/Magic-orig/${key}/`);
+        const dbRef = ref(getDatabase(), `OLN-assets/Magic-orig/${key}/`);
         await set(dbRef, ret[key])
     }
 
